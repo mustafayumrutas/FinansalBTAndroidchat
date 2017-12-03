dfc81f18ff0e15614b35660aa381325d2e136cd8
mustafayumrutas <yumrutas.mustafa@outlook.com>
2017-12-03 21:37:35 +0300

    Giris yapma kısmı tamamen çalışır hale geldi

--- firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/LoginActivity.java
+++ firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/LoginActivity.java
@@ -1,13 +1,101 @@
 package com.example.odev.firebaseandroidchat;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
+    private Toolbar lToolbar;
+    private TextInputLayout lLoginEmail;
+    private TextInputLayout lLoginPassword;
+
+    private Button lLoginbtn;
+
+    private ProgressDialog lLoginprogress;
+
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        lToolbar=(Toolbar) findViewById(R.id.login_toolbar);
+        setSupportActionBar(lToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Giris Yap");
+
+
+
+        lLoginEmail=(TextInputLayout) findViewById(R.id.login_email);
+        lLoginPassword =(TextInputLayout) findViewById(R.id.login_password);
+        lLoginbtn=(Button)findViewById(R.id.login_create_btn);
+
+        lLoginprogress= new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        lLoginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = lLoginEmail.getEditText().getText().toString();
+                String password = lLoginPassword.getEditText().getText().toString();
+
+                if(!TextUtils.isEmpty(email)||!TextUtils.isEmpty(password)){
+                    lLoginprogress.setTitle("Giris Yapılıyor");
+                    lLoginprogress.setMessage("Lütfen bekleyiniz");
+                    lLoginprogress.setCanceledOnTouchOutside(false);
+                    lLoginprogress.show();
+                    loginUser(email,password);
+                }
+            }
+        });
+
+
+    }
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    lLoginprogress.dismiss();
+                    Intent mainIntent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(mainIntent);
+
+                }else{
+                    lLoginprogress.hide();
+                    String TAG = "FIREBASE_EXCEPTION";
+                    FirebaseException e = (FirebaseException)task.getException();
+                    Log.d(TAG, "Reason: " +  e.getMessage());
+                    Toast.makeText(LoginActivity.this, "Hata",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
--- firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/RegisterActivity.java
+++ firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -59,22 +60,37 @@ public class RegisterActivity extends AppCompatActivity {
                 String email =mEmail.getEditText().getText().toString();
                 String password=mPassword.getEditText().getText().toString();
 
-                register_user(display_name,email,password);
+                if (!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                    mRegProgress.setTitle("Uyelik olusturuluyor");
+                    mRegProgress.setMessage("Lütfen üyelik oluşturulurken bekleyin");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+
+
+                    register_user(display_name,email,password);
+
+                }
+
             }
         });
+    }
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
     private void register_user(String display_name,String email,String password){
         mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-
+                    mRegProgress.dismiss();
                     Intent mainIntent =new Intent(RegisterActivity.this,MainActivity.class);
                     startActivity(mainIntent);
                     finish();
 
                 }
                 else{
+                    mRegProgress.hide();
                     String TAG = "FIREBASE_EXCEPTION";
                     FirebaseException e = (FirebaseException)task.getException();
                     Log.d(TAG, "Reason: " +  e.getMessage());
