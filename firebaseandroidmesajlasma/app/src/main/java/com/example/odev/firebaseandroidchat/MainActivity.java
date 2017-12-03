aa25746cf35e855097ff882dab841dbb75489034
mustafayumrutas <yumrutas.mustafa@outlook.com>
2017-12-03 20:00:36 +0300

    Menuye butonlar eklendi
    Menuye butonlar eklendi çıkış butonunun yapısı hazırlandı fakat ui güncellemesi beklemede

--- firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/MainActivity.java
+++ firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -34,5 +36,25 @@ public class MainActivity extends AppCompatActivity {
             finish();
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()==R.id.main_logout_btn){
+            FirebaseAuth.getInstance().signOut();
+        }
+
+
+        return true;
     }
 }
