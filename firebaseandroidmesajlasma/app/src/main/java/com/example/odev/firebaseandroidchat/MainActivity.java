5d386c0288ecdb2d8d84d4ec8161f34df9d7678c
mustafayumrutas <yumrutas.mustafa@outlook.com>
2017-12-08 16:50:24 +0300

    Revert "user profiles"
    This reverts commit 0c14899d9be46e535bbcbc651250f4f4fa33006d.

--- firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/MainActivity.java
+++ firebaseandroidmesajlasma/app/src/main/java/com/example/odev/firebaseandroidchat/MainActivity.java
@@ -79,14 +79,7 @@ public class MainActivity extends AppCompatActivity {
             Intent settingsintent =new Intent(MainActivity.this,SettingsActivity.class);
             startActivity(settingsintent);
         }
-        if(item.getItemId()==R.id.main_all_btn){
-            Intent usersintent=new Intent (MainActivity.this,UsersActivity.class);
-            startActivity(usersintent);
-        }
-        if(item.getItemId()==R.id.main_all_btn){
-            Intent settingsintent =new Intent(MainActivity.this,UsersActivity.class);
-            startActivity(settingsintent);
-        }
+
 
         return true;
     }
